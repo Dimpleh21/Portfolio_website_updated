a424fffefb68e51389ed6fbc3dc88cbaa9c27d52
Dimpleh21 <harjani.dimps143@gmail.com>
2024-09-06 23:32:26 +0530

    gitchange

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {FiFigma} from 'react-icons/fi'
-
-
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/dimple-harjani-3bab1b220/" target='_blank'><BsLinkedin/></a>
-        <a href=" https://github.com/Dimpleh21" target='_blank'><FaGithub/></a>
-        <a href="https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1226568408242300561" target='_blank'><FiFigma/></a>
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/dimple-harjani-652a6925a/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href=" https://github.com/Dimpleh21" target="_blank">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1226568408242300561"
+        target="_blank"
+      >
+        <FiFigma />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
